5ee3e56e20899c2f062a856d93a31c8920da4142
Irwcaro <irwinng300@hotmail.com>
2025-07-21 23:01:35 -0600

    Tarea 4 Completa

--- BDR_Tarea_4.sql
+++ BDR_Tarea_4.sql
@@ -1,49 +1,62 @@
+DROP database if exists tarea_4;
+ 
+create database tarea_4;
+ 
+use tarea_4;
+ 
+drop table if exists AGENTE;
 CREATE TABLE AGENTE(
 id_agente INT PRIMARY KEY AUTO_INCREMENT,
 nombre VARCHAR(50) NOT NULL,
 apellido_paterno VARCHAR(50),
 apellido_materno VARCHAR(50)
 );
-
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (1, 'Pascual', 'Salvador', 'Tormo');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (2, 'Amancio','Estrada' ,'Huertas');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (3, 'Morena','Uribe' , 'Castañeda');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (4, 'Felix', 'Mora' ,'Galán');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (5, 'Camilo' ,'Barreda', 'Bustamante');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (6, 'Crescencia', 'Milla' ,'Requena');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (7, 'Aurelio' ,'Ferreras' ,'Valero');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (8, 'Enrique', 'Mosquera', 'Batalla');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (9, 'Alejo', 'Sainz' ,'Benítez');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (10, 'Maximiliano', 'Tena' ,'Villalobos');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (11, 'Liliana', 'Sureda' ,'Meléndez');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (12, 'Azucena' ,'Uribe' ,'Franco');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (13, 'Marciano' ,'Alberdi', 'Santamaria');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (14, 'Dani', 'Barrio', 'Prat');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (15, 'Teo' , 'Ocaña' , 'Montoya');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (16, 'Osvaldo' , 'Cortina' , 'Palmer');
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES (17, 'Marcelo' , 'Llopis' , 'Priego');
-
-INSERT INTO Agente (id_agente, nombre, apellido_paterno, apellido_materno) VALUES 
-(18, 'Julio', 'Cabrera' ,'Bermejo'),
-(19, 'Maria' , 'Montero' , 'Almagro'),
-(20, 'Felisa' , 'Barral' , 'Tudela');
-
+ 
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Pascual', 'Salvador', 'Tormo');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Pascual', 'Salvador', 'Tormo');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Amancio','Estrada' ,'Huertas');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Morena','Uribe' , 'Castañeda');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Felix', 'Mora' ,'Galán');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Camilo' ,'Barreda', 'Bustamante');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Crescencia', 'Milla' ,'Requena');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Aurelio' ,'Ferreras' ,'Valero');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Enrique', 'Mosquera', 'Batalla');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Alejo', 'Sainz' ,'Benítez');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Maximiliano', 'Tena' ,'Villalobos');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Liliana', 'Sureda' ,'Meléndez');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Azucena' ,'Uribe' ,'Franco');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Marciano' ,'Alberdi', 'Santamaria');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Dani', 'Barrio', 'Prat');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Teo' , 'Ocaña' , 'Montoya');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Osvaldo' , 'Cortina' , 'Palmer');
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES ('Marcelo' , 'Llopis' , 'Priego');
+ 
+INSERT INTO Agente (nombre, apellido_paterno, apellido_materno) VALUES
+('Julio', 'Cabrera' ,'Bermejo'),
+('Maria' , 'Montero' , 'Almagro'),
+('Felisa' , 'Barral' , 'Tudela');
+ 
 select * from  Agente;
-
-
-CREATE TABLE NOMBRE_DE_LA_CUENTA(
-id_agente INT PRIMARY KEY,
+ 
+drop table if exists CUENTA;
+CREATE TABLE CUENTA(
+id_cuenta INT PRIMARY KEY AUTO_INCREMENT,
 nombre_cliente VARCHAR(50) NOT NULL,
 apellido_paterno VARCHAR(50),
 apellido_materno VARCHAR(50),
 folio_fecha DATE,
-FOREIGN KEY (id_agente) REFERENCES AGENTE(id_agente)
+fk_agente INT,
+FOREIGN KEY (fk_agente) REFERENCES AGENTE(id_agente)
 );
-
+ 
+drop table if exists PROPUESTA;
 CREATE TABLE PROPUESTA(
+id_propuesta INT PRIMARY KEY AUTO_INCREMENT,
 fecha_fin DATE,
 observaciones VARCHAR(200),
 prima_neta DECIMAL(10,2),
-FOREIGN KEY (id_agente) REFERENCES AGENTE(id_agente),
-FOREIGN KEY (nombre_cliente) REFERENCES NOMBRE_DE_LA_CUENTA(nombre_cliente)
+fk_agente INT,
+FOREIGN KEY (FK_agente) REFERENCES AGENTE(id_agente),
+fk_cliente INT,
+FOREIGN KEY (fk_cliente) REFERENCES CUENTA(id_cuenta)
 );
